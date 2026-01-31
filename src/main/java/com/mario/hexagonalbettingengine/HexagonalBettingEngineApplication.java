e0dd444dc5cee86fd1911ceadf8221d838f8aa31
Mario <crositecom@gmail.com>
2026-01-31 18:40:16 +0100

    Initialize project structure with Hexagonal Architecture placeholders

---
+++ src/main/java/com/mario/hexagonalbettingengine/HexagonalBettingEngineApplication.java
@@ -0,0 +1,13 @@
+package com.mario.hexagonalbettingengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalBettingEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HexagonalBettingEngineApplication.class, args);
+    }
+
+}
